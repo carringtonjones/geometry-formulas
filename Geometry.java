cabebe76cb09b628af587f5d6a5f4dd094111069
Carrington Jones <cjones6096@greenvilleschools.us>
2017-08-30 10:31:00 -0400

    Added Parallelogram Area

--- Geometry.java
+++ Geometry.java
@@ -24,16 +24,25 @@ public class Geometry
     {
         return 4.0 / 3.0 * Math.PI * Math.pow(radius, 3);
     }
-    
+    public static double parallelogramArea(double base, double height)
+    {
+        double area = base * height;
+        return area;
+    }
     public static void main(String[] args) 
     {
         double ta1 = triangleArea(4.2, 8.1);
         double ta2 = triangleArea(3.0, 123.157);
         
-        System.out.println(ta1);
-        System.out.println(ta2);
+        System.out.println("Triangle #1 Area: " + ta1);
+        System.out.println("Triangle #2 Area: " + ta2);
         
         double ra1 = rectangleArea(5.2, 9.3);
-        System.out.println(ra1);
+        System.out.println("Rectangle Area: " + ra1);
+        
+        double pa1 = parallelogramArea(7,12);
+        System.out.println("Parallelogram Area: " + pa1);
+        
+        
     }
 }
